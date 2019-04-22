0325a5523c877cd87e7d8221204056afa1bd46cc
Romel Gomez <bmxandcode@gmail.com>
2019-04-22 14:05:48 -0500

    configurar-paginas-de-error

--- src/main.py
+++ src/main.py
@@ -4,6 +4,11 @@ app = Flask(__name__)
 
 todos = ['TODO 1', 'TODO 3' , 'TODO 3']
 
+@app.errorhandler(404)
+def not_found(error):
+    return render_template('404.html', error=error)
+
+
 @app.route('/')
 def index():
     user_ip = request.remote_addr
