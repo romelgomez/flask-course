7a7b4d4166b387c8434f83593e13dfe3fe6a6076
Romel Gomez <bmxandcode@gmail.com>
2019-04-22 11:01:47 -0500

    create virtual env

---
+++ dev.sh
@@ -0,0 +1,28 @@
+#!/bin/bash
+
+# 
+# This program created an isolated environment to have all dependencies 
+#
+
+# Get in PROJECT_FOLDER const the current folder where this program is currently running
+PROJECT_FOLDER="$(pwd)"
+
+VENV_DIR=${PROJECT_FOLDER}/./venv
+
+# Create virtual environment directory 
+python3 -m venv ./venv
+
+# Check for virtual environment directory 
+if [ -d "$VENV_DIR" ]; then
+    # Active virtual environment 
+    . ./venv/bin/activate
+    # Install requirements
+    pip install -r ./requirements.txt
+    # Upgrade Pip
+    pip install --upgrade pip
+    python3 --version
+else
+    echo ERROR: ./venv it\'s not a valid folder
+    exit 1
+fi
+ 
